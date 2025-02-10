f1d62f7928891f7c9fe85fa5bb42c62c916ce64c
thiagorfreitas <77907084+thiagorfreitas@users.noreply.github.com>
2025-02-10 20:49:44 -0300

    Tentativa 1955 - Erro

--- 1955_Count_Special_Subsequences/1955.py
+++ 1955_Count_Special_Subsequences/1955.py
@@ -4,4 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        mod = 10**9 + 7
+        count_0, count_1, count_2 = 0, 0, 0
         
+        for num in nums:
+            if num == 0:
+                count_0 = (count_0 + 1) % mod
+            elif num == 1:
+                count_1 = (count_1 + 1) % mod
+            elif num == 2:
+                count_2 = (count_2 + 1) % mod
+                
+        return count_2
